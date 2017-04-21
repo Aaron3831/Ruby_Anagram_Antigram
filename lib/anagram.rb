860b983d66cd349dc2a24dd4d055fd614b1ca640
Aaron Nguyen <aaron3831@gmail.com>
2017-04-21 10:44:56 -0700

    refactor code to include false input

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,7 @@
 class String
   define_method(:anagram) do
 
-    anagram = "cars scar".split(' ')
+    anagram = self.to_str.split(' ')
 
     string1 = [anagram] * " "
 
@@ -9,12 +9,18 @@ class String
     string3 = string2[0]
     string4 = string2[1]
 
-    a = string3
-    b = string4
+    a = string3.downcase
+    b = string4.downcase
 
+    if a.size == b.size && a.delete(b).empty?
+      "This is an Anagram"
+    else
+      "This is NOT an Anagram"
 
-    a.size == b.size && a.delete(b).empty?
+      
+
     #compares length of input a to b after deleting all items from b that are contained in a. empty? confirms if the array is empty afterwards#
+    end
   end
 end
 #use casecmp method for case insensitive comparison#
--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -3,7 +3,9 @@ require('anagram')
 
 describe('String#anagram') do
   it("will determine whether two words are anagrams") do
-    expect(("cars scar").anagram()).to(eq(true))
+    expect(("CARS SCAR").anagram()).to(eq("This is an Anagram"))
   end
-
+  it("will determine whether two words are anagrams") do
+    expect(("blues clues").anagram()).to(eq("This is NOT an Anagram"))
+  end
 end
