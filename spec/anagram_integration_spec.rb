eaac1a29fa14ee6d8dc9ed97859b388ac1c11bfe
Aaron Nguyen <aaron3831@gmail.com>
2017-05-02 17:57:28 -0700

    add passing specs

--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -6,7 +6,8 @@ set(:show_exceptions, false)
 describe('Anagram Logic', {:type => :feature}) do
   it("processes the user's two entries and reveals whether the words are anagrams") do
     visit('/')
-    fill_in('anagram', :with => "hi bye")
+    fill_in('anagram1', :with => "hi")
+    fill_in('anagram2', :with => "bye")
     click_button('Send')
     expect(page).to have_content('This is an antigram')
   end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,18 +3,18 @@ require('anagram')
 
 describe('String#anagram') do
   it("will determine whether two words are anagrams") do
-    expect(("redder redder").anagram()).to(eq("This is an Anagram"))
+    expect(("redder").anagram("redder")).to(eq(["First input is a Palindrome", "Second input is a Palindrome", "This is an Anagram"]))
   end
   it("will determine whether two words are anagrams regardless of letter case") do
-    expect(("REDDER REDDER").anagram()).to(eq("This is an Anagram"))
+    expect(("REDDER").anagram("REDDER")).to(eq(["First input is a Palindrome", "Second input is a Palindrome", "This is an Anagram"]))
   end
   it("will determine whether first word is a palindrome") do
-    expect(("tacocat blahooo").anagram()).to(eq("First input is a Palindrome"))
+    expect(("tacocat").anagram("blahoo")).to(eq(["This is an antigram", "First input is a Palindrome", "First input is a Palindrome"]))
   end
   it("will determine whether second word is a palindrome") do
-    expect(("blahoo tacocat").anagram()).to(eq("Second input is a Palindrome"))
+    expect(("blahoo").anagram("tacocat")).to(eq(["This is an antigram", "Second input is a Palindrome", "Second input is a Palindrome"]))
   end
   it("will determine whether there're NO matches") do
-    expect(("hi bye").anagram()).to(eq("This is an antigram"))
+    expect(("hi").anagram("bye")).to(eq(["This is an antigram", "Not an anagram"]))
   end
 end
